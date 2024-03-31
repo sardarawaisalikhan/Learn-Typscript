d05742f58c1d13fb1497f754fc75d4bc78772a6c
Awais Ali Khan <sardarawais@gmail.com>
2024-03-31 15:12:52 +0500

    This is Awais Ali Khan

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+let x = 2;
+let y = 3;
+console.log(x + y + "xy" + x + y);
+let n = 5;
+console.log(n);
+if ((n = 5)) {
+    console.log("HI");
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+let x = 2;
+let y = 3;
+console.log(x + y + "xy" + x + y);
+
+let n = 5;
+console.log(n);
+
+if ((n = 5)) {
+  console.log("HI");
+}
